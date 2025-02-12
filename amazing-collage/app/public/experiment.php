12cc437c9203b9101f1367df3cfaacc8bf22a960
Volodymyr Sych <wolodymyrs@gmail.com>
2025-02-12 14:10:13 +0200

    amaizing college training site

---
+++ amazing-collage/app/public/experiment.php
@@ -0,0 +1,9 @@
+<?php echo 2 + 2; 
+    $myname = 'Modest';
+?>
+
+<h1>This is an experiment text about <?php echo $myname ?>!</h1>
+
+<?php echo 5 * 5 ?>
+
+<h2>That is an another title about <?php echo $myname ?></h2>
